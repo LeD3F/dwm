81a4c9688ac100acb6ff920f26989689011fadb4
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-11-12 13:20:51 +0300

    Create autostart.sh

---
+++ .dwm/autostart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Apps
+picom --experimental-backends &
+nitrogen --restore &
+xsetroot -cursor_name left_ptr &
+numlockx on &
+greenclip daemon &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+easyeffects --gapplication-service &
+pamac-tray &
+
+#Nice Ones
+~/bin/lock.sh &
+eww daemon &
+
+#Notifications and statusbar
+killall dunst
+dunst -verbosity mesg &
+killall dwmblocks
+dwmblocks &
+
+#Monitor settings
+xrandr --output HDMI-0 --mode 1440x900 -r 75 --pos 0x180 --rotate normal --output DP-4 --primary --mode 1920x1080 -r 144 --pos 1440x0 --rotate normal &
