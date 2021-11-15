5227bd884e4ed49e1553dc6e7e073b0c2a5261e3
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-11-15 21:05:40 +0300

    Add files via upload

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -27,49 +27,8 @@ static const char *fonts[]               = { "JetBrainsMono Nerd Font:size=10:st
                                              "Font Awesome 5 Free:size=20:style=bold:antialias=true:autohint=true",
                                              "NotoEmoji Nerd Font:size=12:antialias=true:autohint=true" };
 
-static char c000000[]                    = "#bc7ad9"; // placeholder value
-
-static char normfgcolor[]                = "#efefef";
-static char normbgcolor[]                = "#1e222a";
-static char normbordercolor[]            = "#282c34";
-static char normfloatcolor[]             = "#1e222a";
-
-static char selfgcolor[]                 = "#ffffff";
-static char selbgcolor[]                 = "#4d78cc";
-static char selbordercolor[]             = "#4d78cc";
-static char selfloatcolor[]              = "#4d78cc";
-
-static char titlenormfgcolor[]           = "#efefef";
-static char titlenormbgcolor[]           = "#1e222a";
-static char titlenormbordercolor[]       = "#282c34";
-static char titlenormfloatcolor[]        = "#1e222a";
-
-static char titleselfgcolor[]            = "#98c379";
-static char titleselbgcolor[]            = "#1e222a";
-static char titleselbordercolor[]        = "#1e222a";
-static char titleselfloatcolor[]         = "#4d78cc";
-
-static char tagsnormfgcolor[]            = "#efefef";
-static char tagsnormbgcolor[]            = "#1e222a";
-static char tagsnormbordercolor[]        = "#282c34";
-static char tagsnormfloatcolor[]         = "#1e222a";
-
-static char tagsselfgcolor[]             = "#71abeb";
-static char tagsselbgcolor[]             = "#282c34";
-static char tagsselbordercolor[]         = "#71abeb";
-static char tagsselfloatcolor[]          = "#71abeb";
-
-static char hidnormfgcolor[]             = "#e06c75";
-static char hidselfgcolor[]              = "#71abeb";
-static char hidnormbgcolor[]             = "#1e222a";
-static char hidselbgcolor[]              = "#1e222a";
-
-static char urgfgcolor[]                 = "#efefef";
-static char urgbgcolor[]                 = "#1e222a";
-static char urgbordercolor[]             = "#6bb2c0";
-static char urgfloatcolor[]              = "#1e222a";
-
-
+// theme
+#include "themes/tokyo.h"
 
 static char *colors[][ColCount] = {
 	/*                       fg                bg                border                float */
@@ -84,18 +43,6 @@ static char *colors[][ColCount] = {
 	[SchemeUrg]          = { urgfgcolor,       urgbgcolor,       urgbordercolor,       urgfloatcolor },
 };
 
-static char *statuscolors[][ColCount] = {
-	/*                       fg                bg                border                float */
-	[SchemeNorm]         = { "#e06c75",        normbgcolor,			normbordercolor,      normfloatcolor },
-	[SchemeSel]          = { "#c9866f",        normbordercolor,		selbordercolor,       selfloatcolor },
-	[SchemeTitleNorm]    = { "#39D7E5",        normbgcolor,  		titlenormbordercolor, titlenormfloatcolor },
-	[SchemeTitleSel]     = { "#6bb2c0",        normbordercolor,		titleselbordercolor,  titleselfloatcolor },
-	[SchemeTagsNorm]     = { "#ebcb8d",        normbgcolor,			tagsnormbordercolor,  tagsnormfloatcolor },
-	[SchemeTagsSel]      = { "#bc7ad9",        normbordercolor,		tagsselbordercolor,   tagsselfloatcolor },
-	[SchemeHidNorm]      = { "#71abeb",        normbgcolor,			c000000,              c000000 },
-	[SchemeHidSel]       = { "#9ec07c",        normbordercolor,		c000000,              c000000 },
-	[SchemeUrg]          = { "#4d78cc",        normbgcolor,			urgbordercolor,       urgfloatcolor },
-};
 
 
 
