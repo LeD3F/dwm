71bb323cca6f3dbdfac2be5012b15155ac577967
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-11-15 21:11:01 +0300

    Add files via upload

---
+++ st/themes/onedark.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+  	"#1e2127",
+  	"#e06c75",
+  	"#98c379",
+  	"#d19a66",
+  	"#61afef",
+  	"#c678dd",
+  	"#56b6c2",
+	"#828791",
+
+	/* bright colors */
+
+  	"#5c6370",
+  	"#e06c75",
+  	"#98c379",
+  	"#d19a66",
+  	"#61afef",
+  	"#c678dd",
+  	"#56b6c2",
+  	"#e6efff",
+
+  	[255] = 0,
+
+  	/* more colors can be added after 255 to use with DefaultXX */
+  	"#add8e6", /* 256 -> cursor */
+  	"#555555", /* 257 -> rev cursor*/
+  	"#1e2127", /* 258 -> bg */
+  	"#ffffff", /* 259 -> fg */
+};
---
+++ st/themes/tokyo.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+  	"#15161E",
+  	"#f7768e",
+  	"#9ece6a",
+  	"#e0af68",
+  	"#7aa2f7",
+  	"#bb9af7",
+  	"#7dcfff",
+	"#a9b1d6",
+
+	/* bright colors */
+
+  	"#414868",
+  	"#f7768e",
+  	"#9ece6a",
+  	"#e0af68",
+  	"#7aa2f7",
+  	"#bb9af7",
+  	"#7dcfff",
+  	"#c0caf5",
+
+  	[255] = 0,
+
+  	/* more colors can be added after 255 to use with DefaultXX */
+  	"#7dcfff", /* 256 -> cursor */
+  	"#a9b1d6", /* 257 -> rev cursor*/
+  	"#15161E", /* 258 -> bg */
+  	"#c0caf5", /* 259 -> fg */
+};
