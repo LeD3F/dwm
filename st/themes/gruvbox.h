a4d5193bedc3a54e3218d618066ec8cf5e9b2eab
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-11-16 15:07:15 +0300

    Add files via upload

---
+++ st/themes/gruvbox.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+  	"#1d2021",
+  	"#fb4934",
+  	"#b8bb26",
+  	"#fabd2f",
+  	"#83a598",
+  	"#d3869b",
+  	"#8ec07c",
+	"#f9f5d7",
+
+	/* bright colors */
+
+  	"#32302f",
+  	"#fb4934",
+  	"#b8bb26",
+  	"#fabd2f",
+  	"#83a598",
+  	"#d3869b",
+  	"#8ec07c",
+  	"#f9f5d7",
+
+  	[255] = 0,
+
+  	/* more colors can be added after 255 to use with DefaultXX */
+  	"#076678", /* 256 -> cursor */
+  	"#fbf1c7", /* 257 -> rev cursor*/
+  	"#1d2021", /* 258 -> bg */
+  	"#f9f5d7", /* 259 -> fg */
+};
