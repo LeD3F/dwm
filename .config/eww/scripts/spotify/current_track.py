123666af7d8c976a98718d755603c27a6589729f
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-11-30 18:47:38 +0300

    Add files via upload

---
+++ .config/eww/scripts/spotify/current_track.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import spotipy
+import spotipy.util as util
+import json
+
+username = "led3f"
+scope = "user-read-currently-playing"
+# scope = 'user-read-playback-state'
+# works as well
+
+token = util.prompt_for_user_token(
+    username,
+    scope,
+    client_id="",
+    client_secret="",
+    redirect_uri="",
+)
+
+spotify = spotipy.Spotify(auth=token)
+current_track = spotify.current_user_playing_track()
+
+print(json.dumps(current_track))
